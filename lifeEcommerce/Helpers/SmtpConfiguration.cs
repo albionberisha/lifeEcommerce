1de2083b9796602eca3ed9b34b992cedb61f77e5
Albion Berisha <albion.b@gjirafa.com>
2022-11-18 10:52:00 +0100

    Remove Already Declared Member

--- lifeEcommerce/Helpers/SmtpConfiguration.cs
+++ lifeEcommerce/Helpers/SmtpConfiguration.cs
@@ -4,7 +4,6 @@
     {
         public string Host { get; set; }
         public string From { get; set; }
-        public string Host { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         public int Port { get; set; } = 587; // default smtp port
