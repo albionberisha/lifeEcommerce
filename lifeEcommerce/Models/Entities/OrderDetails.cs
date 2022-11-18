36fdafa49eca9b306c3d66f6e62cb4b592bd2540
Albion Berisha <albion.b@gjirafa.com>
2022-11-18 10:41:56 +0100

    Order and Shopping Card

---
+++ lifeEcommerce/Models/Entities/OrderDetails.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lifeEcommerce.Models.Entities
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+
+        public string OrderId { get; set; }
+        [ForeignKey("OrderId")]
+        //public OrderData OrderData { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int Count { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
